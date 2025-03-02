df6c2ccaa971d0d2b9392852282ac089397b748e
tailuge <tailuge@gmail.com>
2025-03-02 09:52:50 +0000

    Add environment logging and update script source in documentation HTML

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+console.log("App Environment:", process.env.NODE_ENV);
+
 createRoot(document.getElementById("root")!).render(<App />);
